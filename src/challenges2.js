42ecf6168b6a89abc09a84fc9455da7f5d58034b
Danilo Argolo <dan_argolo@hotmail.com>
2022-08-25 04:41:57 -0300

    Resolução quesito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+//Referência: https://www.youtube.com/watch?v=3lOVeAaytMI
+function generatePhoneNumber(arr) {
+  let contagem = {};
+  let converter = arr.join('');
+  arr.forEach((count) => {
+    contagem[count] = (contagem[count] || 0) + 1;
+  });
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (Math.min(...arr) < 0 || Math.max(...arr) > 9) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  if (Math.max(...Object.values(contagem)) >= 3) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  return converter.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
 }
 
 // Desafio 12
